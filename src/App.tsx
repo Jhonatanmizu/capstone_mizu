48838d71db51305fc7e773ee127652fc851dd88c
jhonatanmizu <natanjesuss20@gmail.com>
2024-08-03 20:10:24 -0300

    :recycle: refactor: remove mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { ICategory } from "@/common/types";
 import { CategoryList } from "./common";
-
-const categories: ICategory[] = [
-  { id: 1, title: "Hats", description: "Shop now" },
-  { id: 2, title: "Jackets", description: "Shop now" },
-  { id: 3, title: "Sneakers", description: "Shop now" },
-  { id: 4, title: "Womens", description: "Shop now" },
-  { id: 5, title: "Mens", description: "Shop now" },
-];
 
 const App = () => {
   return (
     <main className="categories-container">
-      <CategoryList categories={categories} />
+      <CategoryList />
     </main>
   );
 };
